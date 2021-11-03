cc227cf64449299c66babb604083052286e45d82
Martin Chaperot-Merino <m.artin.chapino@gmail.com>
2021-11-03 08:57:50 +0000

    Organize Files

---
+++ Assets/Scripts2/Structures/SpeedBoost.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour
+{
+    public int time = 2;
+    public float thrustPercent = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GliderController controller = other.gameObject.GetComponentInParent<GliderController>();
+            controller.SetThrust(thrustPercent, time);
+        }
+    }
+}
